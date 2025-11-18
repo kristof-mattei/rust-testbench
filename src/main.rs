af71000c271686d7058f8f37f397d3777ecbbff3
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-11-18 11:51:37 -0700

    chore: branch coverage test

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,26 @@ fn print_header() {
     );
 }
 
+fn three_branches(parameter1: bool, parameter2: i32) -> bool {
+    if parameter1 || parameter2 == 5 {
+        println!("TRUE");
+
+        true
+    } else {
+        println!("FALSE");
+
+        false
+    }
+}
+
 fn main() -> Result<(), eyre::Report> {
     HookBuilder::default()
         .capture_span_trace_by_default(true)
         .install()?;
 
     print_header();
+
+    println!("{:?}", three_branches(true, 5));
 
     println!("{}", foo());
     println!("{}", bar());
@@ -55,6 +69,7 @@ fn main() -> Result<(), eyre::Report> {
 #[cfg(test)]
 mod tests {
     use super::{bar, foo, quz};
+    use crate::three_branches;
 
     #[test]
     fn assert_foo() {
@@ -74,5 +89,33 @@ mod tests {
     #[test]
     fn assert_combined() {
         assert_eq!(format!("{}-{}-{}", foo(), bar(), quz()), "Foo-Bar-Quz");
+    }
+
+    #[test]
+    fn three_branches_false_1() {
+        let result = three_branches(false, 1);
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn three_branches_true_1() {
+        let result = three_branches(true, 1);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn three_branches_true_5() {
+        let result = three_branches(true, 5);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn three_branches_false_5() {
+        let result = three_branches(false, 5);
+
+        assert!(result);
     }
 }
